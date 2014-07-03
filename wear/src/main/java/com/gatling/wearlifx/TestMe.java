72bfcb4ea28d3f78b231b06fda395225f4f65142
kentarosu <gimmiepepsi@gmail.com>
2014-07-02 21:48:33 -0400

    Now has a dimmed state.

--- wear/src/main/java/com/gatling/wearlifx/TestMe.java
+++ wear/src/main/java/com/gatling/wearlifx/TestMe.java
@@ -3,16 +3,34 @@ package com.gatling.wearlifx;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 /**
  * Created by gimmiepepsi on 7/2/14.
  */
 public class TestMe extends Activity {
 
+    ImageView watchface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.watchface);
         Log.v("Tavonwear", "the watchface is showing");
+
+        watchface = (ImageView) findViewById(R.id.watchface);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        watchface.setImageResource(R.drawable.ic_launcher);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        watchface.setImageResource(R.drawable.tensecondslater);
     }
 }
